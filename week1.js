5328f2b8090d7a2ae4599f89d5ff60bb4532bc20
Takii-252 <siromaru87@gmail.com>
2025-12-08 00:03:27 +0900

    変数と定数の使い方を学習

---
+++ week1.js
@@ -0,0 +1,22 @@
+console.log("いちご" + "大福");
+console.log("20" + "15");
+
+let name = "にんじゃわんこ";
+console.log(name);
+
+let length = 5;
+console.log(length);
+console.log(length * length * 3);
+
+length = 10;
+console.log(length);
+console.log(length * length * 3);
+
+length += 10;
+console.log(length);
+length /= 10;
+console.log(length);
+
+const yoru = "ねむい";
+console.log(yoru);
+console.log(yoru + "です");
