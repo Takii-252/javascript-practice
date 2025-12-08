2e22b09435f6da0972c90b9b50ebcf396192d784
Takii-252 <siromaru87@gmail.com>
2025-12-08 14:36:51 +0900

    条件分岐学習1

--- week1.js
+++ week1.js
@@ -20,3 +20,37 @@ console.log(length);
 const yoru = "ねむい";
 console.log(yoru);
 console.log(yoru + "です");
+
+const aka = "たけし";
+const ao = "20";
+console.log(`僕の名前は${aka}です`);
+console.log(`僕は${ao}歳です`); //バッククォーテーションを使用しドルと中括弧で囲む
+
+//ここから有料
+//条件分岐
+//学習1 if
+const midori = 15;
+if (midori > 10) {
+  console.log("私はみどりです");
+} //if文の最後にセミコロンはいらない
+
+//学習2 大なり小なり
+console.log(midori > 10);
+console.log(midori < 9);
+
+//学習3 真偽
+const kiiro = "anime";
+if (kiiro === "anime") {
+  console.log("アニメだよ");
+}
+if (kiiro !== "anime") {
+  console.log("アニメじゃないよ");
+}
+
+//学習4 else
+const age2 = 15;
+if (age2 > 10) {
+  console.log("年上ですね");
+} else {
+  console.log("年下ですね");
+}
