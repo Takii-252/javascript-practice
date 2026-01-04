df108e810e042845581639e8789d1eeee15c0998
Takii-252 <siromaru87@gmail.com>
2026-01-04 23:23:33 +0900

    week1の復習2

--- week4.js
+++ week4.js
@@ -15,3 +15,22 @@ for (let i = 0; i < characters.length; i++) {
     console.log(`${character.name}は、まだ新人だ`);
   }
 }
+
+const items = [
+  { name: "やくそう", price: 20, isRare: false },
+  { name: "どくけしそう", price: 30, isRare: false },
+  { name: "せいすい", price: 100, isRare: true },
+  { name: "けんじゃのいし", price: 10000, isRare: true },
+];
+
+for (let i = 0; i < items.length; i++) {
+  console.log("======");
+
+  const item = items[i];
+
+  console.log(`アイテム名：${item.name}`);
+
+  if (item.isRare === true && item.price > 500) {
+    console.log("これは、とても高価なレアアイテムだ！");
+  }
+}
